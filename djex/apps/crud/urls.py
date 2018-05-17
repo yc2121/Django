2655057c9365842da50a65682a7a455ef0d7b58b
yc2121 <yc2121@live.com>
2018-05-17 07:38:59 -0500

    more

---
+++ djex/apps/crud/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+  url(r'^$', views.index),
+  url(r'^register$', views.register),
+  url(r'^login$', views.login),
+  # url(r'^users$', views.users),
+  # url(r'^users/(?P<userid>\d+)$', views.users_details),
+  # url(r'^farm$', views.farm),
+  # url(r'^cove$', views.cove),
+  # url(r'^house$', views.house),
+  # url(r'^casino$', views.casino),
+
+]
