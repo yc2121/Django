050babcc082f243b03da4c11a12307e7a4bb50e0
yc2121 <yc2121@live.com>
2018-05-20 08:03:01 -0500

    more

--- appt/appointments/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-class Appointment(models.Model):
-    datetime = models.DateTimeField()
-    description = models.TextField()
-
-    def __str__(self):
-        return self.description
