0563259b4f38c9ec01d76f1fa01e5cb1f6cdefc1
yc2121 <yc2121@live.com>
2018-05-26 17:08:28 -0500

    more

---
+++ _apps.website-scaffold/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from . import views
+   
+urlpatterns = [
+  url(r'^$', views.index),
+  # url(r'^register/$', views.register),
+  url(r'^login/$', views.login),
+  url(r'^logout/$', views.logout),
+  url(r'^register/$', views.register),
+  url(r'^user/\d+/$', views.info),
+  # url(r'^wishlist_items/(?P<itemID>\d+)/$', views.add),
+  # url(r'^wishlist_items/(?P<itemID>\d+)/view/$', views.view),
+  # url(r'^wishlist_items/(?P<itemID>\d+)/remove/$', views.delete),
+  # url(r'^wishlist_items/create/$', views.create),
+  # url(r'^wishlist_items/new/$', views.newitem),
+]
