516a9072146b87bd5ea0cd77207963dd2c9bc18f
yc2121 <yc2121@live.com>
2018-06-01 07:16:54 -0500

    more

--- crud/apps/website/urls.py
+++ crud/apps/website/urls.py
@@ -3,7 +3,6 @@ from . import views
    
 urlpatterns = [
   url(r'^$', views.index),
-  # url(r'^register/$', views.register),
   url(r'^login/$', views.login),
   url(r'^logout/$', views.logout),
   url(r'^register/$', views.register),
